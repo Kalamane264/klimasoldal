a3be4ad6b5e01f3b7cbb0444fd606c5336928dc4
Kalman Varga <kalamane007@gmail.com>
2026-08-05 12:32:46 +0200

    product og image fix

--- app/lib/seo-helpers.ts
+++ app/lib/seo-helpers.ts
@@ -20,7 +20,7 @@ export async function getProductMetadata(id: string, urlBeginsWith: string): Pro
       url: urlBeginsWith + id,
       images: [
         {
-          url: product.pics[0].src,
+          url: "/pic/" + product.pics[0].src,
           width: product.pics[0].width,
           height: product.pics[0].height,
           alt: data.fullName,
